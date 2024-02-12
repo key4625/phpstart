720fd26c8ef6735f717ff979c168302437ae21c1
key4625 <michele.cappannari@gmail.com>
2024-02-12 13:02:49 +0100

    nuovo php

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,14 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?= $siteName ?><?= isset($title) ? " - ".$title : ""; ?></title>
+    <title>
+    <?php 
+      echo $siteName; 
+      if (isset($title)){ 
+        echo " - ".$title;
+      } else echo ""; 
+    ?>
+    </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
     <link rel="styesheet" href="/css/style.css">
